cae6b66eaca7eed00ce8126235c2493f81d0a8e4
Abner2111 <abneraq73@gmail.com>
2018-05-08 13:54:01 -0600

    agregue todos los atributos de la clase juego y cambie el color del fondo de la interfaz

--- Prueba.py
+++ Prueba.py
@@ -1,13 +1,18 @@
+import tkinter.messagebox
 from tkinter import *
 
 pantalla = Tk()
 pantalla.minsize(800, 500)
 pantalla.title("PONG")
-pantalla.mainloop()
+pantalla.configure(bg="black")
 
 
 class Juego:
-    def __init__(self):
+    puntaje = []
+    modo = ''
+    paletas = 1
+    nivel = 1
+    def __init__(self, modo, paletas):
         self.matriz = [ [False, False, False, False, False, False, False, False, False, False,
                         False, False, False, False, False, False, False, False, False, False,
                         False, False, False, False, False, False, False, False, False, False,
@@ -108,4 +113,15 @@ class Juego:
                         False, False, False, False, False, False, False, False, False, False,
                         False, False, False, False, False, False, False, False, False, False,
                         False, False, False, False, False, False, False, False, False, False] ]
+        self.Puntaje = puntaje
+        self.modo = modo
+        self.paletas = paletas
+        self.nivel = nivel
+    def set_nivel(self):
+        self.nivel = 1#entrada del programa
+        return self.nivel
 
+    def cambiar_nivel(self):
+        if puntaje[0] == 7 or puntaje[1] == 7:
+            set_nivel()
+pantalla.mainloop()
