8ff6842b782ca38bb921ef77402af611f0c4ea19
Abner2111 <abneraq73@gmail.com>
2018-05-08 18:02:40 -0600

    Agregue clase JUgador y le puse que fuera los pads

--- Prueba.py
+++ Prueba.py
@@ -1,11 +1,30 @@
 import tkinter.messagebox
 from tkinter import *
-
+import time
+HEIGHT=500
+WIDTH = 800
 pantalla = Tk()
-pantalla.minsize(800, 500)
+pantalla.minsize(WIDTH, HEIGHT)
 pantalla.title("PONG")
-pantalla.configure(bg="black")
 
+c = Canvas(pantalla, width=WIDTH, height = HEIGHT, bg = "black")
+c.pack()
+
+
+p1 = [0, 20]
+p2 = [780, 800]
+
+class Jugador:
+    velocidad = 1
+    color_paleta = "black"
+    def __init__(self, size,player):
+        self.shape = c.create_rectangle(player[0], 0,player[1] , size, fill="white")
+
+
+        """self.color_paleta = ""
+        self.velocidad = velocidad
+        self.posiciony =
+        self.posicionx ="""
 
 class Juego:
 
@@ -127,3 +146,7 @@ class Juego:
         if ((self.puntaje[0] == 7 or self.puntaje[1] == 7) and self.nivel<=3):
             self.nivel += 1
 
+pad1 = Jugador(180, p1)
+pad2 = Jugador(180,p2)
+
+pantalla.mainloop()
