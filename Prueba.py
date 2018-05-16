69653a88cde5f2c31fc9eae8e5e3ad033c30ff2a
fabian152195 <fabian152195@gmail.com>
2018-05-16 11:28:17 -0600

    movimiento paleta modifica matriz

--- Prueba.py
+++ Prueba.py
@@ -67,7 +67,9 @@ class paleta:
         self.canvas = canvas
         self.shape = canvas.create_rectangle(width[0], height[0], width[1], height[1], fill='white')
         self.posicion = canvas.coords(self.shape)
+        print('este',self.posicion)
         self.y = 0
+        print('este', lado[0],lado[1])
         self.canvas.bind_all(lado[0], self.up)
         self.canvas.bind_all(lado[1], self.down)
 
@@ -78,26 +80,25 @@ class paleta:
     def mover(self):
         self.canvas.move(self.shape, 0, self.y)
         self.set_posicion(0,self.y,0,self.y)
-        Juego.modificar_matriz(Juego, self.get_posicion())
+        Juego.modificar_matriz(Juego, self.get_posicion(),self.y)
 #        Juego.hace_Cero(Juego, self.get_posicion())
         pos = self.posicion
-        if pos[1] < 0:
+        if pos[1] <= 0:
             self.y = 0
-        if pos[3] > 500:
+        if pos[3] >= 500:
             self.y = 0
     def set_tamano(self,height):
         self.shape = c.create_rectangle(0, height[0], 20, height[1], fill='white')
-
     def set_color(self,color):
         self.shape = c.create_rectangle(self.width[0], self.height[0], self.width[1], self.height[1], fill=str(color))
 
     def get_posicion(self):
         return self.posicion
     def set_posicion(self, x1,y1,x2,y2):
-        self.posicion[0]+=x1
-        self.posicion[1]+=y1
-        self.posicion[2]+=x2
-        self.posicion[3]+=y2
+        self.posicion[0] += x1
+        self.posicion[1] += y1
+        self.posicion[2] += x2
+        self.posicion[3] += y2
         return self.posicion
 
 class Jugador(paleta):
@@ -163,10 +164,16 @@ class Juego:
         if ((self.puntaje[0] == 7 or self.puntaje[1] == 7) and self.nivel<=3):
             self.nivel += 1
 
-    def modificar_matriz(self, pos):
-        for i in range(int(pos[1])//20, int(pos[3])//20-1):
+    def modificar_matriz(self, pos,cero):
+        if cero == 20:
+            self.matriz[(int(pos[1]//20))-1][int(pos[0])//20]*=0
+        elif cero == -20:
+            self.matriz[(int(pos[3])//20)][int(pos[0]) // 20]*=0
+        for i in range(int(pos[1])//20, int(pos[3])//20):
             for j in range(int(pos[0])//20, int(pos[2])//20):
                 self.matriz[i][j] = 1
+        return self.matriz
+
     def hace_Cero(self, pos):
         for i in range(26):
             for j in range(41):
@@ -176,8 +183,10 @@ class Juego:
 
 
 def prnt_m(matriz):
+    print('\n--------')
     for elemento in matriz:
         print(elemento)
+'''
 def mv_dn(Juego, clase, objeto):
     clase.mover_paletasDown(clase, objeto)
     Juego.modificar_matriz(Juego, objeto.get_posicion())
@@ -187,24 +196,23 @@ def mv_up(Juego, clase, objeto):
     Juego.modificar_matriz(Juego, objeto.get_posicion())
     prnt_m(Juego.matriz)
 #JuegoPrincipal = Juego()
-
+'''
 
 
 
 pad1 = paleta(c, grande, p1, lado=izquierda)
 pad2 = paleta(c, grande, p2, lado=derecha)
-Juego.modificar_matriz(Juego, pad1.get_posicion())
-Juego.modificar_matriz(Juego, pad2.get_posicion())
+#Juego.modificar_matriz(Juego, pad1.get_posicion())
+#Juego.modificar_matriz(Juego, pad2.get_posicion())
 
 
 bola = Bolita()
-
-prnt_m(Juego.matriz)
 
 while True:
     pad1.mover()
     pad2.mover()
     bola.move()
+    print('matrizjuego')
     prnt_m(Juego.matriz)
     pantalla.update()
     time.sleep(0.05)
